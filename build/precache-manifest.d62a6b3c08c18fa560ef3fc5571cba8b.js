8041304282deff9071dfaf5a8e674c4f194aa698
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-28 23:29:13 -0400

    prod build

---
+++ build/precache-manifest.d62a6b3c08c18fa560ef3fc5571cba8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c25b6aab25035cdaf09da7c6924551f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87ccd9c3e25ae7b0eeb2",
+    "url": "/static/css/2.bc03ab51.chunk.css"
+  },
+  {
+    "revision": "32dbbc4a215cb683dfdc",
+    "url": "/static/css/main.1931a0c3.chunk.css"
+  },
+  {
+    "revision": "87ccd9c3e25ae7b0eeb2",
+    "url": "/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "32dbbc4a215cb683dfdc",
+    "url": "/static/js/main.f83a79d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f07ebb82ce2d95df14a41dca1239d3ef",
+    "url": "/static/media/banner.f07ebb82.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
