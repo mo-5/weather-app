345d6bdd3587f5846f3f25eef268aaff97fe228c
Mohamed Radwan <mohamed.rad@outlook.com>
2019-07-29 23:56:11 -0400

    home page change

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/share/nginx/html/weather/precache-manifest.df5ed8df0f97b18fbd8cccf338c42b27.js"
+  "./precache-manifest.692dc4645c6b38a22c4c520067cc63d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/share/nginx/html/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
